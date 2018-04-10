105ace1c9c64d58d414ad64ba7b7ef2c80192033
Kasper Holbek Jensen <kholbekj@gmail.com>
2018-04-11 00:05:22 +0200

    Cleanup doing

--- mush/commands/doing.rb
+++ mush/commands/doing.rb
@@ -1,21 +1,12 @@
 require_relative 'command'
 class Doing < Command
   NAME = "doing"
-
-  PREFIXES = ['doing', '@doing']
-  SHORTCUT = nil
-
   HELP = "doing <message> - Sets your \"doing\" message, as seen in the \"who\" command."
 
   def process(thing, command)
 		message = @parts[1..-1].join(' ')
 		thing.doing = message
 		thing.save
-		return("Doing set: #{message}\n")
+		"Doing set: #{message}\n"
 	end
-
-	def name
-		return NAME
-	end
-
 end
