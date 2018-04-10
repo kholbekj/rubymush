5bce30868ff49122245ab25276d032443741d066
Kasper Holbek Jensen <kholbekj@gmail.com>
2018-04-10 23:58:50 +0200

    Cleanup create

--- mush/commands/create.rb
+++ mush/commands/create.rb
@@ -1,18 +1,10 @@
 require_relative 'command'
 class Create < Command
   NAME = "create"
-
-  PREFIXES = ['create']
-  SHORTCUT = nil
-
   HELP = "create <name> - Creates a new object and places it in your inventory"
 
   def process(thing, command)
     new_thing = Thing.create(location_id: thing.id, owner_id: thing.id, kind: 'object', name: command.split(' ')[1..-1].join(' '), created_at: Time.now)
-    return("Object created: #{new_thing.name} (##{new_thing.id})\n")
-  end
-
-  def name
-    return NAME
+    "Object created: #{new_thing.name} (##{new_thing.id})\n"
   end
 end
