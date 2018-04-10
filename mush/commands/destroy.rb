1a9697c022c769d6d1642305fb3e6fdc3a3dc029
Kasper Holbek Jensen <kholbekj@gmail.com>
2018-04-11 00:04:21 +0200

    Cleanup destroy

--- mush/commands/destroy.rb
+++ mush/commands/destroy.rb
@@ -1,32 +1,18 @@
 require_relative 'command'
 class Destroy < Command
   NAME = "destroy"
-
-  PREFIXES = ['destroy', 'dest']
-  SHORTCUT = nil
-
   HELP = "destroy <ref> - Destroys an object"
 
 	def process(thing, command)
-		if @parts.size == 2
-			t = find_thing(thing, @parts[1..-1].join(' '))
-			if t
-				if t.user_can_edit?(thing) and t != thing
-					t.destroy
-					return("#{t.name_ref} destroyed!\n")
-				else
-					return("Permission denied.\n")
-				end
-			else
-				return("Object not found!\n")
-			end
-		else
-			return("Destroy requires an object number.\n")
-		end
-	end
+    return "Destroy requires an object number.\n" unless @parts.size == 2
 
-	def name
-		return NAME
-	end
+    t = find_thing(thing, @parts[1..-1].join(' '))
 
+    return "Object not found.\n" unless t
+    return "Permission denied.\n" unless t.user_can_edit?(thing)
+
+    t.destroy
+
+    "#{t.name_ref} destroyed!\n"
+  end
 end
