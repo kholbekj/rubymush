3848268e50ff78a1051ab573169d83a17690be89
Kasper Holbek Jensen <kholbekj@gmail.com>
2018-04-11 00:01:47 +0200

    Cleanup describe

--- mush/commands/describe.rb
+++ mush/commands/describe.rb
@@ -1,36 +1,21 @@
 require_relative 'command'
 class Describe < Command
   NAME = "describe"
-
-  PREFIXES = ['describe', 'desc']
-  SHORTCUT = nil
-
   HELP = "describe <ref>=<description> - Sets the description of an object"
 
   def process(thing, command)
-    if @parts.size >= 2
-      @parts.shift
-      @parts = @parts.join(' ').split('=')
-      t = find_thing(thing, @parts[0])
-      if t
-        if t.user_can_edit?(thing)
-          t.description = @parts[1..-1].join('=').strip
-          t.save
-          return("Description set for #{t.name_ref}.\n")
-        else
-          return("Permission denied.\n")
-        end
-      else
-        return("Object not found.\n")
-      end
+    return "Usage: description <object name or ref>=<description>\n" unless @parts.size >= 2
 
-    else
-      return("Usage: description <object name or ref>=<description>\n")
-    end
+    @parts.shift
+    @parts = @parts.join(' ').split('=')
+    t = find_thing(thing, @parts[0])
+
+    return "Object not found.\n" unless t
+    return "Permission denied.\n" unless t.user_can_edit?(thing)
+
+    t.description = @parts[1..-1].join('=').strip
+    t.save
+
+    "Description set for #{t.name_ref}.\n"
   end
-
-  def name
-    return NAME
-  end
-
 end
